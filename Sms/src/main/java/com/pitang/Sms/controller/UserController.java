26f47f8a45ea68975755469dea0688fc75e476bc
Gabriel Cezario <GabrielCezarioR@gmail.com>
2020-03-01 00:07:14 -0300

    Commit 01/03 : Adding ResponseEntity in post method

--- Sms/src/main/java/com/pitang/Sms/controller/UserController.java
+++ Sms/src/main/java/com/pitang/Sms/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.pitang.Sms.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,8 +23,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, path = "/addUser")
-	public UserDto addUser(@RequestBody UserDto userDto) {
-		
+	public ResponseEntity<?> addUser(@RequestBody UserDto userDto) {
 		UserModel userModel = ModelMapperComponent.modelMapper.map(userDto, UserModel.class);
 		ModelMapperComponent.modelMapper.validate();
 		
@@ -30,8 +31,8 @@ public class UserController {
 		
 		userDto = ModelMapperComponent.modelMapper.map(userModel, UserDto.class);
 		ModelMapperComponent.modelMapper.validate();
-		
-		return userDto;
+				
+		return new ResponseEntity<>(userDto, HttpStatus.OK);
 	}
 
 }
